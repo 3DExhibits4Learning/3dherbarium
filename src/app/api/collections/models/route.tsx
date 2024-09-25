d852191223f18560a0fd9f046a22d7611c2ef55a
AJ <ab632@humboldt.edu>
2024-09-25 14:44:34 -0700

    swapped nodeEnv for custom env

--- src/app/api/collections/models/route.tsx
+++ src/app/api/collections/models/route.tsx
@@ -1,10 +1,10 @@
 import { getAllSiteReadyModels } from "@/api/queries"
 
 // Returns all SiteReadyModels
-export async function GET(request: Request) {
-      console.log('TEST: The following should be true: ', !!(process.env.NODE_ENV === 'development'))
+export async function GET() {
+      console.log('TEST: The following should be true: ', !!(process.env.LOCAL_ENV === 'development'))
       try{
-        const models = await getAllSiteReadyModels(!!(process.env.NODE_ENV === 'development'))
+        const models = await getAllSiteReadyModels(!!(process.env.LOCAL_ENV === 'development'))
         return Response.json({data: 'models got', response: models})
       }
       catch(e: any) {return Response.json({data:'Error getting models', response: e.message}, {status: 400, statusText: 'Error getting models'})}
