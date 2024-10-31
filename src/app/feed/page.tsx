c8e6c332f5263a2598e8a2180c39f205797fc310
AJ <ab632@humboldt.edu>
2024-10-30 22:31:11 -0700

    feed update

--- src/app/feed/page.tsx
+++ src/app/feed/page.tsx
@@ -2,17 +2,17 @@
 
 import Header from "@/components/Header/Header"
 import Foot from "@/components/Shared/Foot"
-//import ModelViewer from "@/components/Dashboard/ModelViewer"
-import dynamic from "next/dynamic"
-const ModelViewer = dynamic(() => import('../../components/Shared/ModelViewer'), { ssr: false })
+import { Divider } from "@nextui-org/react"
 
 export default function Page() {
     return (
         <>
             <Header pageRoute="collections" headerTitle='feed' />
+            <p className="mt-8 ml-4 mb-2">10-31-2024</p>
+            <Divider />
             <main className="min-h-[calc(100vh-177px)] h-fit flex justify-center">
                 <article className='w-4/5 h-fit text-center flex flex-col items-center'>
-                    <p className="text-3xl mt-12">Welcome to the new 3D Digital Herbarium Feed!</p>
+                    <p className="text-3xl mt-8">Welcome to the new 3D Digital Herbarium Feed!</p>
                     <br></br><br></br>
                     <p className="text-lg">This is where all of the latest news regarding the herbarium will be posted, beginning with the welcome of the Fall 2024 student team:</p>
                     <br></br>
@@ -21,6 +21,8 @@ export default function Page() {
                     <p><span className="font-medium">Simon Saltikov</span>: Programming Assistant</p>
                     <br></br><br></br>
                     <p className="text-lg">Check back here regularly for updates!</p>
+                    <br></br>
+                    <p className="text-lg">*Note that the site may be offline periodically over the next day or two while we stabalize updates</p>
                 </article>
             </main>
             <Foot />
