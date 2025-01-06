888d5c7f6128a50baa0639f4f4d55905bf96d419
AJ <ab632@humboldt.edu>
2025-01-06 11:31:19 -0800

    dark mode tiles updated

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -13,17 +13,16 @@ import 'leaflet/dist/leaflet.css';
 
 export default function Map({ center, children }: { center: LatLngLiteral, children: ReactNode }) {
 
-  // This clause ensures that this code doesn't run server side; it will throw an error if it does (it uses the window object)
   if (typeof window !== 'undefined') {
 
-    const lightModeTiles: string = 'https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}.png';
-    const darkModeTiles: string = 'https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Dark_Gray_Base/MapServer/tile/{z}/{y}/{x}';
-    const openAttribution: string = '&copy; https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+    const lightModeTiles: string = 'https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}.png'
+    const darkModeTiles: string = 'https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png'
+    const darkAttribution: string = "&copy; <a href='https://www.openstreetmap.org/' rel='noopener noreferrer'>OpenStreetMap</a> contributors, &copy; <a href='https://carto.com/' rel='noopener noreferrer'>CartoDB</a>"
     const esriAttribution: string = "Powered by <a href='https://www.esri.com/en-us/home' rel='noopener noreferrer'>Esri</a>"
     const prefersDarkMode: boolean = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
 
     let tiles = !prefersDarkMode ? lightModeTiles : darkModeTiles
-    let attribution = !prefersDarkMode ? openAttribution : esriAttribution
+    let attribution = !prefersDarkMode ? esriAttribution: darkAttribution
 
     return (
       <MapContainer className='h-full w-full' center={[center.lat, center.lng]} zoom={10} scrollWheelZoom={false}>
