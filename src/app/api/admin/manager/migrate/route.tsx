e13a56f8fff4ebfdd182cb7a1fdd3e3801874411
AJ <ab632@humboldt.edu>
2025-01-28 08:20:39 -0800

    independent data migrations added

--- src/app/api/admin/manager/migrate/route.tsx
+++ src/app/api/admin/manager/migrate/route.tsx
@@ -8,7 +8,6 @@
 import prisma from "@/utils/prisma"
 import * as migrate from '@/functions/server/migration'
 import { routeHandlerErrorHandler, routeHandlerTypicalCatch } from "@/functions/server/error"
-import { routeHandlerTypicalResponse } from "@/functions/server/response"
 
 // PATH
 const path = 'src/app/api/admin/manager/migrate/route.tsx'
@@ -20,12 +19,13 @@ const path = 'src/app/api/admin/manager/migrate/route.tsx'
 export async function POST() {
 
     try {
+        
         // Environment (thus database) determinations
         const local = process.env.LOCAL_ENV
         const db = local === 'development' ? 'Development' : 'Test'
         const target = local === 'development' ? 'Test' : 'Production'
 
-        // Array of data migrations for transaction
+        // Array of data migrations for transaction (d => t)
         const modelMigrationArray = [
             migrate.speciesMigration,
             migrate.specimenMigration,
@@ -37,8 +37,20 @@ export async function POST() {
             migrate.modelAnnotationMigration,
         ]
 
+        // Array of data migrations for transaction (t => p)
+        const productionModelMigrationArray = [
+            migrate.productionSpeciesMigration,
+            migrate.productionSpecimenMigration,
+            migrate.productionModelMigration,
+            migrate.productionImageSetMigration,
+            migrate.productionAnnotationMigration,
+            migrate.productionPhotoAnnotationMigration,
+            migrate.productionVideoAnnotationMigration,
+            migrate.productionModelAnnotationMigration,
+        ]
+
         // Transaction 
-        await prisma.$transaction(modelMigrationArray).catch(e => routeHandlerErrorHandler(path, e.message, 'prisma.$transaction()', "Couldn't migrate annotated 3D models"))
+        await prisma.$transaction(db === "Development" ? modelMigrationArray : productionModelMigrationArray).catch(e => routeHandlerErrorHandler(path, e.message, 'prisma.$transaction()', "Couldn't migrate annotated 3D models"))
 
         // Basic response
         return new Response(`Annotated 3D models from ${db} database have been migrated to ${target} database`)
