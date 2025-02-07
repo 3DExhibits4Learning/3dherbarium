9a294f4ac8dae5fdd3f3ae3ab0b9f85e4dec2f16
AJ <ab632@humboldt.edu>
2025-02-07 12:51:45 -0800

    test

--- src/components/Search/SearchPageModelList.tsx
+++ src/components/Search/SearchPageModelList.tsx
@@ -3,8 +3,8 @@
  * 
  * @fileoverview search client component listing all site ready 3D models
  * 
- * @todo determine where community models are leaking into the herbarium model array
- * @todo handle duplicates pushed onto community array allowing set to be removed
+ * @todo determine where community models are leaking into the filteredModel array before they are supposed to be pushed
+ * @todo handle duplicates pushed onto filtredModel array allowing duplicate check to be removed
  */
 
 // Typical imports
@@ -19,6 +19,7 @@ import { sortModelsByOrder } from '@/functions/client/search'
 import HerbariumCard from './HerbariumCard'
 import CommunityCard from './CommunityCard'
 
+// Main JSX
 export default function SearchPageModelList(props: { state: SearchPageState, setState: Dispatch<SetStateAction<SearchPageState>>, models: model[] }) {
 
   // Props => variables
@@ -40,17 +41,14 @@ export default function SearchPageModelList(props: { state: SearchPageState, set
     filteredModels = filteredModels.filter(model => !Object.keys(model).includes('confirmation')) // Need to figure out where a community model is sneaking into this array which should only have herbarium models at this point
     filteredModels = filteredModels.filter(model => (model as model).spec_name.toLowerCase().includes(query.toLowerCase()) || (model as model).pref_comm_name.toLowerCase().includes(query.toLowerCase()))
     communityModels = communityModels.filter(model => model.speciesName.toLowerCase().includes(query.toLowerCase()) || model.commonName.toLowerCase().includes(query.toLowerCase()))
-    communityModels = Array.from(new Set(communityModels.map(model => JSON.stringify(model)).map(string => JSON.parse(string))))
   }
 
-  // Join herbarium models and community models if there is no selected modeler or annotator and the corresponding checkbox is checked; sort models by order
-  communityModels = Array.from(new Set(communityModels.map(model => JSON.stringify(model)).map(string => JSON.parse(string))))
+  // A bug is causing community model duplicates when a query is started and then deleted; this is the temporary patch
   filteredModels = Array.from(new Set(filteredModels.map(model => JSON.stringify(model)).map(string => JSON.parse(string))))
+
+  // Join herbarium models and community models if there is no selected modeler or annotator and the corresponding checkbox is checked; sort models by order
   if (selectedModeler === 'All' && selectedAnnotator === 'All' && state.communityIncluded) filteredModels.push(...communityModels)
   filteredModels = sortModelsByOrder(filteredModels, state.order as 'Newest First' | 'Alphabetical' | 'Reverse Alphabetical')
-
-  console.log(communityModels)
-  console.log(filteredModels)
 
   return <>
     {
